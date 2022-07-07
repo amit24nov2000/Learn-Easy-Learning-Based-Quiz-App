753fcd1108b82f8acb5ddedb629fb36a32b9a1e5
Amit kumar <amit24nov2000@gmail.com>
2022-07-07 11:46:14 +0530

    Learning Based quiz app

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -10,77 +10,131 @@ object Constants {
         val questionList=ArrayList<Question>()
 
         val que1=Question(
-            1,"what country does this flag belong to?",
-            R.drawable.flag_of_argentina,
-            "Argentina","Australia","India","Armenia",
+            1,"Identify the object",
+            R.drawable.flag_of_india,
+            "ध्वज","स्टिच","पुस्तक","कर्गजम्",
             1
 
         )
         questionList.add(que1)
 
         val que2=Question(
-            1,"what country does this flag belong to?",
-            R.drawable.flag_of_belgium,
-            "Argentina","Australia","belgium","Armenia",
+            1,"Identify the object",
+            R.drawable.book,
+            "हेर","स्टिच","पुस्तक","कर्गजम्",
             3
 
         )
         questionList.add(que2)
 
         val que3=Question(
-            1,"what country does this flag belong to?",
-            R.drawable.flag_of_denmark,
-            "Argentina","Australia","India","denmark",
-            4
+            1,"Identify the object",
+            R.drawable.apple,
+            "सेवफल","स्टिच", "नारङ्ग","कर्गजम्",
+            1
 
         )
         questionList.add(que3)
 
         val que4=Question(
-            1,"what country does this flag belong to?",
-            R.drawable.flag_of_fiji,
-            "Argentina","Australia","India","fiji",
-            4
+            1,"Identify the object",
+            R.drawable.birds,
+            "हेर", "पक्षिणः","पुस्तक","कर्गजम्",
+            2
 
         )
         questionList.add(que4)
 
         val que5=Question(
-            1,"what country does this flag belong to?",
-            R.drawable.flag_of_germany,
-            "Argentina","germany","India","Armenia",
-            2
+            1,"Identify the object",
+            R.drawable.bottle,
+            "हेर","स्टिच","पुस्तक","कूपी",
+            4
 
         )
         questionList.add(que5)
 
         val que6=Question(
-            1,"what country does this flag belong to?",
-            R.drawable.flag_of_india,
-            "Argentina","Australia","India","Armenia",
+            1,"Identify the object",
+            R.drawable.bulb,
+            "हेर","स्टिच","बल्बः","कर्गजम्",
             3
 
         )
         questionList.add(que6)
 
         val que7=Question(
-            1,"what country does this flag belong to?",
-            R.drawable.flag_of_kuwait,
-            "Argentina","fiji","India","kuwait",
-            4
+            1,"Identify the object",
+            R.drawable.chair,
+            "आसन्द","स्टिच","पुस्तक","कर्गजम्",
+            1
 
         )
         questionList.add(que7)
 
         val que8=Question(
-            1,"what country does this flag belong to?",
-            R.drawable.ic_flag_of_brazil,
-            "Argentina","Australia","India","Brazil",
+            1,"Identify the object",
+            R.drawable.clock,
+            "हेर","स्टिच","पुस्तक",
+                    "घटिका",
             4
 
         )
-
         questionList.add(que8)
+
+        val que9=Question(
+            1,"Identify the object",
+            R.drawable.comb,
+            "हेर","स्टिच","कङ्कत","कर्गजम्",
+            3
+
+        )
+        questionList.add(que9)
+
+        val que10=Question(
+            1,"Identify the object",
+            R.drawable.crocodile,
+            "मीन","मकरः","पुस्तक","कर्गजम्",
+            2
+
+        )
+        questionList.add(que10)
+
+        val que11=Question(
+            1,"Identify the object",
+            R.drawable.fish,
+            "मीन","मकरः","पुस्तक","कर्गजम्",
+            1
+
+        )
+        questionList.add(que11)
+
+        val que12=Question(
+            1,"Identify the object",
+            R.drawable.frog,
+            "हेर","मंडूक:","पुस्तक","कर्गजम्",
+            2
+
+        )
+        questionList.add(que12)
+
+        val que13=Question(
+            1,"Identify the object",
+            R.drawable.moon,
+            "हेर","शशांक","पुस्तक","सूर्य",
+            2
+
+        )
+        questionList.add(que13)
+
+        val que14=Question(
+            1,"Identify the object",
+            R.drawable.sun,
+            "हेर","स्टिच","सूर्य","कर्गजम्",
+            3
+
+        )
+        questionList.add(que14)
         return questionList
     }
 }
